0237fc04801c0fe076685454f444b874704bde67
OtavioRL <otaviohlopess@gmail.com>
2023-02-09 01:03:15 -0300

    fix docker-compose and connection to db

--- back-end/src/Models/Connection.ts
+++ back-end/src/Models/Connection.ts
@@ -1,7 +1,8 @@
 import mongoose, { Mongoose } from 'mongoose';
 import 'dotenv/config';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/PsiqueDB';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/PsiqueDB';
+
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGOURI || MONGO_DB_URL
