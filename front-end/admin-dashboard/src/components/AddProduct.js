794f969900f1b91a06adc93b4129253f83fb5caf
OtavioRL <otaviohlopess@gmail.com>
2023-02-10 16:24:28 -0300

    form add produtos

--- front-end/admin-dashboard/src/components/AddProduct.js
+++ front-end/admin-dashboard/src/components/AddProduct.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 
-const style = {
+const modalStyle = {
   position: 'absolute',
   top: '50%',
   left: '50%',
@@ -12,17 +13,57 @@ const style = {
   border: '1px solid #000',
   boxShadow: 24,
   p: 4,
+  display: 'flex',
+  flexDirection: 'column'
 };
+
+const inputStyle = {
+  mt: 2
+}
 
 const AddProduct = () => {
   return (
-    <Box sx={style}>
+    <Box sx={modalStyle}>
       <Typography id="modal-modal-title" variant="h6" component="h2">
-        Insira os dados do produto que vai ser inserido
+        Insira os dados do produto que vai ser inserido no banco de dados
       </Typography>
-      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </Typography>
+      <TextField 
+        sx={inputStyle} 
+        id="outlined-basic" 
+        label="Nome" 
+        variant="outlined" 
+      />
+      <TextField
+        sx={inputStyle}
+        id="outlined-multiline-static"
+        label="Descrição"
+        multiline
+        rows={4}
+      />
+      <TextField
+        sx={inputStyle}
+        id="outlined-number"
+        label="Preço"
+        type="number"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <TextField
+        sx={inputStyle}
+        id="outlined-number"
+        label="Estoque"
+        type="number"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <TextField 
+        sx={inputStyle} 
+        id="outlined-basic" 
+        label="Link da imagem" 
+        variant="outlined" 
+      />
     </Box>
   );
 };
